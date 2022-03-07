2571407bec84e46f456db394a3c20b4eba1ea173
we1tingyu <a8755311020@gmail.com>
2022-03-07 11:41:14 +0800

    update

--- app/faceMeshProjectForFlask.py
+++ app/faceMeshProjectForFlask.py
@@ -1,13 +1,3 @@
-#
-# AIEN16 第二組 臉部偵測Sample code --> code reference from google mediapipe website.
-# google MediaPipe - AI機器學習套件介紹, 它的套件包括: 
-# 臉部偵測, 臉部特徵立體偵測, 手部關節運動追蹤, 人像與背景的分割, 髮型特徵分割, 人體立體運動姿勢追蹤, 物件辨識, 
-# 臉/身體/支幹整體偵測, 3D物件偵測.
-# https://google.github.io/mediapipe/
-# google MediaPipe github 
-# https://github.com/google/mediapipe
-#
-
 import cv2
 import mediapipe as mp
 import time
