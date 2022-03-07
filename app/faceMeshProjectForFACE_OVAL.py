1be89ce4794449ca148f07f4b1bb4876285f69cb
we1tingyu <a8755311020@gmail.com>
2022-03-07 12:10:24 +0800

    3/7 bin

---
+++ app/faceMeshProjectForFACE_OVAL.py
@@ -0,0 +1,262 @@
+
+# AIEN16 第二組 臉部偵測Sample code --> git clone from google mediapipe website.
+# google MediaPipe - AI機器學習套件介紹, 它的套件包括: 
+# 臉部偵測, 臉部特徵立體偵測, 手部關節運動追蹤, 人像與背景的分割, 髮型特徵分割, 人體立體運動姿勢追蹤, 物件辨識, 
+# 臉/身體/支幹整體偵測, 3D物件偵測.
+# https://google.github.io/mediapipe/
+# google MediaPipe github 
+# https://github.com/google/mediapipe
+#
+
+import cv2
+import mediapipe as mp
+import time
+import math
+import threading
+import facialFeatures
+ 
+
+# 創建一個lock對象: lock = threading.Lock()
+# 獲取使用lock: lock.acquire()
+# 解除釋放lock: lock.release()
+# 釋放前其他Thread(執行緒)無法執行
+lock = threading.Lock()
+
+
+BLUE = (255, 106 , 90)
+BLACK = (0, 0, 0)
+
+# COLOR 可以由前端按按鈕切換
+DEFAULT_COLOR = BLUE
+
+class FaceMeshDetector():
+    def __init__(self, staticMode=False, maxFaces=10, minDetectionCon=False, minTrackCon=0.5):
+        
+        self.staticMode = staticMode
+        self.maxFaces = maxFaces
+        self.minDetectionCon = minDetectionCon
+        self.minTrackCon = minTrackCon
+        self.mpDraw = mp.solutions.drawing_utils
+        # 畫出臉上的線條
+        self.mpDrawingStyles = mp.solutions.drawing_styles
+        self.mpFaceMesh = mp.solutions.face_mesh
+        self.faceMesh = self.mpFaceMesh.FaceMesh(self.staticMode, self.maxFaces, self.minDetectionCon, self.minTrackCon)
+        # there are 468 points.
+        self.drawSpec = self.mpDraw.DrawingSpec(color=(30,144,255), thickness=1, circle_radius=1) 
+
+    def findFaceMesh(self, img, drawFaceLms=True, drawID=False, drawFortuneTelling=0):
+        # 左右相反，for camera
+        #self.imgRGB = cv2.cvtColor(cv2.flip(img, 1), cv2.COLOR_BGR2RGB) # opposite right/left for actual face sync-up detection when face turns left/right.
+        self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.faceMesh.process(self.imgRGB)
+        # put faces[]/distance{} here to avoid error"local variable referenced before assignment" 
+        faces = []
+        distance = []
+        FACE_OVAL=facialFeatures.FACE_OVAL
+        READ_FACE=facialFeatures.READ_FACE
+        sum=0  
+        # multi_face_landmarks :臉上作完正規化的xyz座標
+        if self.results.multi_face_landmarks:
+            for faceLms in self.results.multi_face_landmarks:                
+                if drawFaceLms:
+                    # image=img, landmark_list=faceLms, connections=self.mpFaceMesh.FACEMESH_TESSELATION, 
+                    # landmark_drawing_spec=self.drawSpec,
+                    # connection_drawing_spec=self.mpDrawingStyles.get_default_face_mesh_tesselation_style().
+                    # 臉上的各點及其連線
+                    self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACEMESH_TESSELATION,
+                                                self.drawSpec, self.mpDrawingStyles.get_default_face_mesh_tesselation_style())
+                    # image=img, landmark_list=faceLms, connections=self.mpFaceMesh.FACEMESH_CONTOURS, 
+                    # landmark_drawing_spec=self.drawSpec,
+                    # connection_drawing_spec=self.mpDrawingStyles.get_default_face_mesh_contours_style().
+                    # goolge劃出的範例線條
+                    # self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACEMESH_CONTOURS,
+                    #                             self.drawSpec, self.mpDrawingStyles.get_default_face_mesh_contours_style())
+                
+                if drawFortuneTelling == 0:
+                    pass
+                
+                elif drawFortuneTelling == 1:
+                    # draw specific IDs for fortune telling(畫出上面自行定義的各點)
+                    # for idx1,value in enumerate(FACE_OVAL):
+                    for idx1,ff in enumerate(FACE_OVAL):
+                        sum=0
+                        for idx2,value in enumerate(FACE_OVAL[idx1]):
+
+                            startID, endID = value
+                            # print(f'startID:{startID} endID:{endID} ')
+                            # faceID to xyz                            
+                            ih, iw, ic = img.shape
+                            lm_start = faceLms.landmark[startID]
+                            # x, y, z = int(lm_start.x*iw), int(lm_start.y*ih), int(lm_start.z*ic)
+                            x1, y1, z1 = lm_start.x*iw, lm_start.y*ih, lm_start.z*ic
+                            # 起點的 2D int 座標 (給 cv2 用)
+                            startAddress2D = int(x1), int(y1)
+                            lm_end = faceLms.landmark[endID]
+                            x2, y2, z2 = lm_end.x*iw, lm_end.y*ih, lm_end.z*ic
+                            # 終點的 2D int 座標 (給 cv2 用)
+                            endAddress2D = int(x2), int(y2)
+                            # print(f'startAddress:{startAddress},endAddress:{endAddress}')
+
+                            # draw specific line user defined (只畫線, 不算距離)
+                            self.drawSpecificLine(img, startAddress2D, endAddress2D)
+
+                            # 起點的 3D int 座標
+                            startAddress3D = x1, y1, z1
+                            # 終點的 3D int 座標
+                            endAddress3D = x2, y2, z2
+
+                            # Euclaidean Distance 計算出距離
+                            # 計算 2D 距離
+                            # lineDistance = self.euclaideanDistance(startAddress2D, endAddress2D)
+                            # 計算 3D 距離
+                            lineDistance = self.euclaideanDistance3D(startAddress3D, endAddress3D)
+                            # lineDistance = int(lineDistance)
+                            # print(f'lineDistance (單站里程):{lineDistance}')
+
+                            sum+=lineDistance
+                            
+                            print(f'FACE_OVAL (目前累積里程):{sum}')    
+                            # append into distance[]
+                            distance.append(lineDistance)
+
+                    print('------')
+                    print(f'FACE_OVAL (總里程):{sum}')
+                    print('------------')
+
+                elif drawFortuneTelling == 2:
+                    # draw specific IDs for fortune telling(畫出上面自行定義的各點)
+                    # READ_FACE=facialFeatures.READ_FACE
+                    for idx1,ff in enumerate(READ_FACE):
+                        sum=0
+                        for idx2,value in enumerate(READ_FACE[idx1]):
+                            startID, endID = value
+                            # print(f'startID:{startID} endID:{endID} ')
+                            # faceID to xyz                            
+                            ih, iw, ic = img.shape
+                            lm_start = faceLms.landmark[startID]
+                            # x, y, z = int(lm_start.x*iw), int(lm_start.y*ih), int(lm_start.z*ic)
+                            x1, y1, z1 = lm_start.x*iw, lm_start.y*ih, lm_start.z*ic
+                            # 起點的 2D int 座標 (給 cv2 用)
+                            startAddress2D = int(x1), int(y1)
+                            lm_end = faceLms.landmark[endID]
+                            x2, y2, z2 = lm_end.x*iw, lm_end.y*ih, lm_end.z*ic
+                            # 終點的 2D int 座標 (給 cv2 用)
+                            endAddress2D = int(x2), int(y2)
+                            # print(f'startAddress:{startAddress},endAddress:{endAddress}')
+
+                            # draw specific line user defined (只畫線, 不算距離)
+                            self.drawSpecificLine(img, startAddress2D, endAddress2D, BLACK)
+                            
+                            # 起點的 3D int 座標
+                            startAddress3D = x1, y1, z1
+                            # 終點的 3D int 座標
+                            endAddress3D = x2, y2, z2
+
+                            # Euclaidean Distance 計算出距離
+                            # 計算 2D 距離
+                            # lineDistance = self.euclaideanDistance(startAddress2D, endAddress2D)
+                            # 計算 3D 距離
+                            lineDistance = self.euclaideanDistance3D(startAddress3D, endAddress3D)
+
+                            # lineDistance = self.drawSpecificLine(img, startAddress, endAddress)
+                            # lineDistance = int(lineDistance)
+                            sum+=lineDistance
+                            # print(lineDistance)
+                            # print(sum)
+                        # append into distance[]                           
+                        distance.append(sum)                     
+                    if distance:
+                        print(f'RIGHT_EYEBROW:{distance[0]}, LEFT_EYEBROW:{distance[1]}, RIGHT_EYE:{distance[2]}, LEFT_EYE{distance[3]}, NOSE_LENGTH:{distance[4]}, NOSE_WIDTH:{distance[5]}, FOREHEAD:{distance[6]}, PHILTRUM:{distance[7]}, MOUTH:{distance[8]}')
+                        
+                face = []
+                for id, lm in enumerate(faceLms.landmark):   # use enumerate to get index and values 
+                    ih, iw, ic = img.shape
+                    x, y, z = int(lm.x*iw), int(lm.y*ih), int(lm.z*ic)
+                    # 畫出臉上每個點的ID
+                    if drawID:
+                        cv2.putText(img, str(id), (x, y), cv2.FONT_HERSHEY_PLAIN, 0.7, (0, 255, 0), 1)  # print each ID on face
+                    #print(lm)           # print x, y, z of each point
+                    #print(id, x, y)
+                    #face.append([x,y])  # save x, y without id. 
+                    face.append([id,x,y,z])
+                faces.append(face)
+        return img, faces, distance
+    
+    # https://google.github.io/mediapipe/solutions/iris.html
+    
+        
+    
+    
+    # Euclaidean Distance
+    def euclaideanDistance(self, startPoint, endPoint):
+        x1, y1 = startPoint
+        x2, y2 = endPoint
+        distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 )
+        return distance
+
+    # Euclaidean Distance
+    def euclaideanDistance3D(self, startPoint, endPoint):
+        x1, y1, z1 = startPoint
+        x2, y2, z2 = endPoint
+        distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2 )
+        return distance
+    
+    # draw specific line user defined
+    def drawSpecificLine(self, img, startAddress, endAddress, color=DEFAULT_COLOR):
+        # address for begin point and destination point
+        self.startAddress = startAddress
+        self.endAddress= endAddress        
+        # using cv2.line to draw line 畫出指定點到點的線
+        cv2.line(img, self.startAddress, self.endAddress, color, 3)        
+
+        # 就不在這裡算距離了 !!!
+        # Euclaidean Distance 計算出距離
+        # LineDistance = self.euclaideanDistance(self.startAddress, self.endAddress)
+        # LineDistance = self.euclaideanDistance3D(self.startAddress, self.endAddress)
+        # return LineDistance
+
+def faceMeshDetection(videoMode=True, filePath="./videos/1-720p.mp4", drawFaceLms=True, drawID=False, drawFortuneTelling=0):
+    #cap = cv2.VideoCapture(0)                         # From camera capture real time 
+    #cap = cv2.VideoCapture("../videos/2-1080p.mp4")   # required AV1 hardware decoding
+    #cap = cv2.VideoCapture("../videos/2-720pL.mp4")
+    #cap = cv2.VideoCapture("../videos/1-720p.mp4")
+    # grab global references to the lock variables
+    global lock
+    if videoMode:
+        cap = cv2.VideoCapture(filePath)
+    pTime = 0
+    detector = FaceMeshDetector(maxFaces=10)
+    #faceDetection = True
+    while True:
+        # success為True/False
+        if videoMode:
+            success, img = cap.read()
+        else:
+            #img = cv2.imread('images/03.png')
+            img = cv2.imread(filePath)
+        # In order to make all video_feed sources working at the same fps, we need to use threading lock. 
+        with lock:                  # acquire threading lock, set the output frame, and release threading lock.  
+            img, faces, distance = detector.findFaceMesh(img, drawFaceLms, drawID, drawFortuneTelling)      
+                    
+            # time.time():1970年之後經過的秒数
+            cTime = time.time()
+            fps = 1/(cTime-pTime)
+            pTime = cTime
+            cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+            #cv2.imshow("Image", img)
+        
+            # 傳送至前端
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        if cv2.waitKey(1) == ord('q'): 
+            break 
+    if videoMode:    
+        cap.release()   
+    cv2.destroyAllWindows()
+    return img, faces, distance
+
+if __name__ == "__main__":
+    faceMeshDetection()
+    
+
+
--- app/main.py
+++ app/main.py
@@ -1,17 +1,36 @@
 # import all necessary 3rd python packages 
 from flask import Flask, render_template, Response
-#import modules.mysql_connection
-from faceMeshProjectForFlask import faceMeshDetection
+# import modules.mysql_connection
+
+from faceMeshProjectForFACE_OVAL import faceMeshDetection
+# from app3 import faceMeshDetection
+
+# from faceMeshProjectForFlask import faceMeshDetection_test
 #
+# Flask 類別 初始化時 傳入的 __name__ 參數，代表當前模組的名稱。
+# 為固定用法，以便讓 Flask 知道在哪裡尋找資源。
+# (例如: 模板和靜態文件)
 app = Flask(__name__, '/')
-#
+
+# 裝飾器是告訴 Flask，哪個 URL 應該觸發我們的函式。
+# 斜線代表的就是網站的根目錄，可以疊加。
+# 例如: 新增一個 /hello 的位址
+# @app.route("/")
+# @app.route("/hello")
+# def hello():
+#     return "Hello, World!"
+# 網站訪問首頁與/hello，呈現的同樣是 hello 函式，回傳的 Hello World 文字。
+#render_template是flask的內建函數，字面上的意思大概是渲染模板，因此它需要一個『模板』來提供渲染的格式。
+# 當return render_template('html文件')的時候，flask會先到專案資料夾『templates』去尋找相對應的html文件，
+# 因此，你需要先做的就是在專案底下建置一個資料夾，並且命名為『templates』
 @app.route('/')
 def index():
     return render_template('faceDetection.html')
-#
+
 # feed a video stream as a source
-@app.route('/video_feed')
-def video_feed():
+# 前端的 video_feed 加上 style 參數, 傳到 drawFortuneTelling, 表示要畫哪一種圖 (0 不畫, 1 畫臉框, 2 畫五官)
+@app.route('/video_feed/<int:style>')
+def video_feed(style):
     # multipart/x-mixed-replace is an HTTP header. Your server can use it to push dynamically updated content to the web browser.
     # It works by telling the browser to keep the connection open and replace the web page or piece of media it is displaying with another when it receives a special token.
     # The header is old and widely supported. It works in legacy browsers.
@@ -20,23 +39,18 @@ def video_feed():
     filePath = "./videos/1-720p.mp4"
     drawFaceLms = True 
     drawID = False 
-    drawFortuneTelling = False 
+    drawFortuneTelling = style
     return Response(faceMeshDetection(videoMode, filePath, drawFaceLms, drawID, drawFortuneTelling),
                 mimetype='multipart/x-mixed-replace; boundary=frame')
+    # mimetype 媒體類別 multipart/x-mixed-replace 資料傳輸格式
 
-@app.route('/video_feed_1')
-def video_feed_1():
-    # multipart/x-mixed-replace is an HTTP header. Your server can use it to push dynamically updated content to the web browser.
-    # It works by telling the browser to keep the connection open and replace the web page or piece of media it is displaying with another when it receives a special token.
-    # The header is old and widely supported. It works in legacy browsers.
-    # Each yield expression is directly sent thru Response to the browser.
-    videoMode = True
-    filePath = "./videos/1-720p.mp4"
-    drawFaceLms = False 
-    drawID = False 
-    drawFortuneTelling = True 
-    return Response(faceMeshDetection(videoMode, filePath, drawFaceLms, drawID, drawFortuneTelling),
-                mimetype='multipart/x-mixed-replace; boundary=frame')
+# 連接AZURE的mysql
+@app.route('/try-mysql')
+def try_mysql():
+    (cursor, cnx) = modules.mysql_connection.get_cursor()
+    sql = ("SELECT * FROM new_table")
+    cursor.execute(sql)
+    return render_template('data_table.html', t_data=cursor.fetchall())
 
 '''
 @app.route('/mysql')
