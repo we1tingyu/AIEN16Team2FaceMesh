3aabb2f4fb16ddf837e663a87f1ff64790fe04f9
we1tingyu <a8755311020@gmail.com>
2022-03-07 13:58:27 +0800

    update

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app = Flask(__name__, '/')
 # 因此，你需要先做的就是在專案底下建置一個資料夾，並且命名為『templates』
 @app.route('/')
 def index():
-    return render_template('faceDetection.html')
+    return render_template('index.html')
 
 # feed a video stream as a source
 # 前端的 video_feed 加上 style 參數, 傳到 drawFortuneTelling, 表示要畫哪一種圖 (0 不畫, 1 畫臉框, 2 畫五官)
