092f01644f68d03e92ac78b1f6132b13ee056200
we1tingyu <a8755311020@gmail.com>
2022-03-24 09:52:15 +0800

    update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 # import all necessary 3rd python packages 
 from copy import copy
-from flask import Flask, render_template, Response, request,redirect,url_for
-from pytube import YouTube
+from flask import Flask, render_template, Response, request, url_for, redirect, flash
 import modules.mysql_connection
 
 from faceMeshProjectForFACE_OVAL import faceMeshDetection, FaceMeshDetector
@@ -11,8 +10,12 @@ from takePictureCountdown import streamlive
 # from takePicture0314_IP import streamlive
 
 import cv2
+import sqlQuery
 
 from flask_table import Table, Col, LinkCol
+
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+import configparser
 
 # from faceMeshProjectForFlask import faceMeshDetection_test
 #
@@ -27,6 +30,88 @@ import flask
 app = flask.Flask(__name__, '/')
 # CORS(app)
 
+### login
+# config 初始化
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# app.secret_key = config.get('flask', 'secret_key')
+app.secret_key = 'dd06be55a06c03312b2ab109b5f8f6ab'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.session_protection = "strong"
+login_manager.login_view = 'login'
+# login_manager.login_message = '請證明你並非來自黑暗草泥馬界'
+login_manager.login_message = 'YAYAYA'
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(使用者):
+    # if 使用者 not in users:
+    if 使用者 is None or sqlQueryMember(使用者, ''):
+        return
+
+    user = User()
+    user.id = 使用者
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    使用者 = request.form.get('user_id')
+    # if 使用者 not in users:
+    if 使用者 is None or sqlQueryMember(使用者, ''):
+        return
+
+    user = User()
+    user.id = 使用者
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    # user.is_authenticated = request.form['password'] == users[使用者]['password']
+    user.is_authenticated = sqlQueryMember(使用者, request.form['password'])
+
+    return user
+
+# users = {'Me': {'password': 'myself'}, '123': {'password': '456'}}
+
+# sql 查詢 member, 回傳 True or False
+def sqlQueryMember(account_number, password):
+    if account_number is None:
+        return False
+    memberExist = sqlQuery.sqlQueryMember(account_number, password)
+    print(memberExist)
+    # return True
+    return memberExist
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    
+    使用者 = request.form['user_id']
+    # if (使用者 in users) and (request.form['password'] == users[使用者]['password']):
+    if sqlQueryMember(使用者, request.form['password']):
+        user = User()
+        user.id = 使用者
+        login_user(user)
+        # flash(f'{使用者}！歡迎加入草泥馬訓練家的行列！')
+        flash(f'{使用者}！YAYAYA！')
+        return redirect(url_for('index'))
+
+    flash('登入失敗了...')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    使用者 = current_user.get_id()
+    logout_user()
+    flash(f'{使用者}！歡迎下次再來！')
+    return render_template('login.html')
+### login 結束
+
 # 裝飾器是告訴 Flask，哪個 URL 應該觸發我們的函式。
 # 斜線代表的就是網站的根目錄，可以疊加。
 # 例如: 新增一個 /hello 的位址
@@ -39,27 +124,20 @@ app = flask.Flask(__name__, '/')
 # 當return render_template('html文件')的時候，flask會先到專案資料夾『templates』去尋找相對應的html文件，
 # 因此，你需要先做的就是在專案底下建置一個資料夾，並且命名為『templates』
 @app.route('/')
+# @login_required
 def index():
-    # return render_template('Home.html')
-    return render_template('Index.html')
+    return render_template('Home.html')
+    # return render_template('Index.html')
     # return render_template('Test.html')
 
 #功能體驗
 @app.route('/Experience')
 def Experience():
-    # if request.method == 'POST':
-    #     url = request.form['url']
-    #     yt = YouTube(url)
-    #     print("開始下載影片")
-    #     yt.streams.get_highest_resolution().download('./videos')
-    #     print("影片下載完成")
-    #     filename=yt.title
-    #     return redirect(url_for('video_feed', style='五眼', videoMode='影片'))
-
     return render_template('Experience.html')
 
 #數據分析
 @app.route('/DataAnalysis')
+# @login_required
 def DataAnalysis():
     return render_template('DataAnalysis.html')  
 
@@ -82,9 +160,6 @@ def Signin():
 @app.route('/Signup')
 def Signup():
     return render_template('Signup.html')
-
-
-
 
 # feed a video stream as a source
 # 前端的 video_feed 加上 style 參數, 傳到 drawFortuneTelling, 表示要畫哪一種圖; 加上 videoMode 參數, 傳到 videoMode, 表示"影片"或"照片"
@@ -109,7 +184,17 @@ def video_feed(style, videoMode):
         videoMode = True
     elif videoMode == "照片":
         # filePath = "app/static/images/Thelatestphotos.jpg"
+        # 大餅臉
         filePath = "app/static/images/Thelatestphotos.jpg"
+        # AngelaBaby
+        # filePath = "app/static/Thelatestphotos7.jpg"
+        # 側臉
+        # filePath = "app/static/Thelatestphotos8.jpg"
+        # "完美大帥哥
+        # filePath = "app/static/handsome.jpg"
+        # 小夫(側臉線條失敗)
+        # filePath = "app/static/Thelatestphotos11.jpg"
+
         videoMode = False
     elif videoMode == "照相":
         return Response(streamlive(style),
